e022f4a1e0853b1a5296756ecf9fcf1db74251da
Jerson <benjimaster2003@gmail.com>
2024-09-24 15:14:19 -0500

    carga del examen parte 2

---
+++ src/test/java/pe/edu/cibertec/carros_backend_b/CarrosBackendBApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.carros_backend_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosBackendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
